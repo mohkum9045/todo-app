bde69f7d1023b9e804d796edab3343f6a59966fe
Mohit <mohkum9045@gmail.com>
2022-09-24 19:13:31 +0530

    ToDo app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from "react";
 
-function App() {
+// const App = () => {
+//   const [items, setitems] = useState();
+//   const [items2, setitems2] = useState([]);
+
+//   const fillChange = (event) => {
+//     setitems(event.target.value);
+//   };
+
+//   const addItems = () => {
+//     setitems2((array) => {
+//       return [...array, items];
+//     });
+//     setitems("");
+//   };
+
+//   const deleteitem = (id) => {
+
+//     setitems2((olditems) => {
+//       return olditems.filter((element, val) => {
+//         return val !== id;
+//       });
+//     });
+//   };
+
+//   return (
+//     <>
+//       <input
+//         placeholder="Enter the Task to do"
+//         value={items}
+//         onChange={fillChange}></input>
+//       <button onClick={addItems}>+</button>
+//       <ul>
+//         {items2.map((val, index) => {
+//           return (
+//             <li>
+//               <button id={index} onClick={deleteitem}>
+//                 -
+//               </button>
+//               {val}
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default App;
+
+import React, { useState } from "react";
+
+const App = () => {
+  const [items, setitems] = useState();
+  const [aItems, asetItems] = useState([]);
+
+  const getItem = (event) => {
+    setitems(event.target.value);
+  };
+
+  const handleItem = () => {
+    asetItems((array) => {
+      return [...array, items];
+    });
+    setitems("");
+  };
+
+  const deleteItems = (event) => {
+    asetItems((oldItems) => {
+      return oldItems.filter((data, ind) => {
+        return ind !== event.target.id;
+      });
+    });
+    console.log(event.target.id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        placeholder="Enter the items into the list"
+        value={items}
+        onChange={getItem}></input>
+      <button onClick={handleItem}>+</button>
+      <ul>
+        {aItems.map((value, index) => {
+          return (
+            <li>
+              <button id={index} onClick={deleteItems}>
+                -
+              </button>
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h1>THINGS TO DO</h1>
+      <input placeholder="Add New"></input>
+    </div>
+  );
+};
+
+export default Main;
